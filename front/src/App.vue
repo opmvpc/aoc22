e88c02e713c489e77644108c69832e39b59ea67f
opmvpc <thibsix@outlook.be>
2022-12-02 04:13:15 +0100

    design

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,18 +3,7 @@ import Bench from "./components/Bench.vue";
 </script>
 
 <template>
-  <div class="bg-gray-900 min-h-screen p-3">
-    <div class="container mx-auto">
-      <header>
-        <nav class="py-4">
-          <h1 class="text-5xl text-gray-200 font-serif font-bold">
-            🎄 AoC 2022
-          </h1>
-        </nav>
-      </header>
-      <main>
-        <Bench />
-      </main>
-    </div>
+  <div class="bg-gray-900 min-h-screen pb-3">
+    <Bench />
   </div>
 </template>
--- front/src/components/Bench.vue
+++ front/src/components/Bench.vue
@@ -11,7 +11,6 @@ const run = async (day: number) => {
   addResult(day, 1, 2);
   addResult(day, 2, 1);
   addResult(day, 2, 2);
-  console.log(results);
 };
 
 const addResult = async (day: number, part: number, fileNumber: number) => {
@@ -25,62 +24,85 @@ const addResult = async (day: number, part: number, fileNumber: number) => {
 const getKey = (day: number, part: number, fileNumber: number) => {
   return `${day}-${part}-${fileNumber}`;
 };
+
+const runAll = () => {
+  for (let day = 1; day <= 25; day++) {
+    run(day);
+  }
+};
 </script>
 
 <template>
-  <div class="flex flex-col space-y-4">
-    <div
-      class="backdrop-blur-lg bg-white/30 shadow-md p-4 rounded-md text-gray-900 flex flex-col space-y-4"
-    >
-      <div class="flex space-x-2">
-        <label class="font-bold" for="times">Times</label>
-        <input
-          class="border border-gray-300 rounded-md px-2 font-mono backdrop-blur-lg bg-white/30"
-          type="number"
-          v-model="times"
-        />
-      </div>
-    </div>
-    <ul class="grid grid-cols-2 gap-4">
-      <li
-        v-for="day in 31"
-        class="backdrop-blur-lg bg-white/30 shadow-md p-4 rounded-md text-gray-900 flex flex-col space-y-4"
+  <header
+    class="mb-4 sticky top-0 left-0 z-10 backdrop-blur-md pt-4 shadow-xl w-full px-3"
+  >
+    <nav class="container mx-auto flex flex-col space-y-4">
+      <h1 class="text-5xl text-gray-200 font-serif font-bold">🎄 AoC 2022</h1>
+      <div
+        class="backdrop-blur-lg bg-white/30 shadow-md p-4 rounded-md text-gray-900 flex justify-between"
       >
-        <div class="font-bold">Day {{ day }}</div>
-        <div v-for="part in 2">
-          <div v-for="fileNumber in 2">
-            <div class="grid grid-cols-5 gap-3">
-              <div>{{ part }} - {{ fileNumber }}</div>
-              <div class="col-span-3 font-mono tabular-nums text-right">
-                {{
-                  results.get(getKey(day, part, fileNumber))?.time.toFixed(6) ??
-                  "?"
-                }}
-                ms
-              </div>
-              <div
-                class="font-mono tabular-nums text-right"
-                :class="
-                  results.get(getKey(day, part, fileNumber))?.result ===
-                  results.get(getKey(day, part, fileNumber))?.expectedResult
-                    ? 'text-green-500'
-                    : 'text-red-500'
-                "
-              >
-                {{ results.get(getKey(day, part, fileNumber))?.result }}
+        <div class="flex space-x-2">
+          <label class="font-bold" for="times">Times</label>
+          <input
+            class="border border-gray-300 rounded-md px-2 font-mono backdrop-blur-lg bg-white/10"
+            type="number"
+            v-model="times"
+          />
+        </div>
+        <button
+          class="backdrop-blur-lg bg-white/10 px-4 py-1 text-sm rounded"
+          @click="runAll()"
+        >
+          Run all
+        </button>
+      </div>
+      <div></div>
+    </nav>
+  </header>
+  <main class="container mx-auto">
+    <div class="flex flex-col space-y-4">
+      <ul class="grid md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <li
+          v-for="day in 25"
+          class="backdrop-blur-lg bg-white/30 shadow-md p-4 rounded-md text-gray-900 flex flex-col space-y-4"
+        >
+          <div class="font-bold">Day {{ day }}</div>
+          <div v-for="part in 2">
+            <div v-for="fileNumber in 2">
+              <div class="grid grid-cols-5 gap-3">
+                <div>{{ part }} - {{ fileNumber }}</div>
+                <div class="col-span-3 font-mono tabular-nums text-right">
+                  {{
+                    results
+                      .get(getKey(day, part, fileNumber))
+                      ?.time.toFixed(6) ?? "?"
+                  }}
+                  ms
+                </div>
+                <div
+                  class="font-mono tabular-nums text-right"
+                  :class="
+                    results.get(getKey(day, part, fileNumber))?.result ===
+                    results.get(getKey(day, part, fileNumber))?.expectedResult
+                      ? 'text-green-500'
+                      : 'text-red-500'
+                  "
+                >
+                  {{ results.get(getKey(day, part, fileNumber))?.result }}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="flex justify-end">
-          <button
-            class="backdrop-blur-lg bg-white/10 px-4 py-1 text-sm rounded"
-            @click="run(day)"
-          >
-            Run
-          </button>
-        </div>
-      </li>
-    </ul>
-  </div>
+          <div class="flex justify-end">
+            <button
+              class="backdrop-blur-lg bg-white/10 px-4 py-1 text-sm rounded"
+              @click="run(day)"
+            >
+              Run
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
