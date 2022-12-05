04d5c15ac2f0989b9ffbd99a64c4c1a86ce71a55
opmvpc <thibsix@outlook.be>
2022-12-05 04:37:48 +0100

    test keypath

---
+++ ecosystem.config.github.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: "aoc22",
+      script: "./build/server.js",
+      instances: "max",
+      exec_mode: "cluster",
+      autorestart: true,
+      watch: true,
+      env: {
+        PORT: 3333,
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      "user": "pm2",
+      "host": ["tsix.be"],
+      "key": "./deploy.key",
+      "ref": "origin/main",
+      "repo": "git@github.com:opmvpc/aoc22.git",
+      "path": "/var/www/pm2/aoc22",
+      "post-deploy": "npm install && npm run build",
+    },
+  },
+};
