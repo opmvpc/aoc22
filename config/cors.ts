72c84c00d1cac2480b19d75b3c694bd21661e4cc
opmvpc <thibsix@outlook.be>
2022-12-05 02:45:36 +0100

    cors

--- config/cors.ts
+++ config/cors.ts
@@ -44,7 +44,7 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin: true,
+  origin: "http://localhost,http://aoc22.tsix.be/,https://aoc22.tsix.be/",
 
   /*
   |--------------------------------------------------------------------------
