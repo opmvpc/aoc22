4783366c379083896c7f338bbee0f02789bd8867
opmvpc <thibsix@outlook.be>
2022-12-06 21:33:54 +0100

    day5 part 2

--- app/Domain/Solvers/5/2.ts
+++ app/Domain/Solvers/5/2.ts
@@ -2,7 +2,7 @@ import Solver from "../Contracts/Solver";
 import parser from "../Parser";
 
 class S implements Solver {
-  public expectedResult: string[] = ["CMZ", "???"];
+  public expectedResult: string[] = ["MCD", "??"];
   public day: number = 5;
   public part: number = 2;
   public inputs: Promise<string>[];
@@ -14,9 +14,57 @@ class S implements Solver {
   }
 
   public async solve(number: number): Promise<string> {
-    const lines = (await this.inputs[number - 1]).split("\n");
+    const lines = (await this.inputs[number - 1]).split("\n\n");
+    const stacks: string[][] = [];
 
-    return "";
+    const config = lines[0].split("\n");
+    for (let index = config.length - 2; index >= 0; index--) {
+      const line = config[index]
+        .replace(/    /g, " ")
+        .split(" ")
+        .map((x) => {
+          return x.charAt(1);
+        });
+
+      for (let index = 0; index < line.length; index++) {
+        const char = line[index];
+        if (stacks[index] === undefined) {
+          stacks[index] = [];
+        }
+        if (char !== "") {
+          stacks[index].push(char);
+        }
+      }
+    }
+
+    const instructions = lines[1].split("\n");
+    const regex = /move (\d+) from (\d+) to (\d+)/;
+    for (let i = 0; i < instructions.length; i++) {
+      const instruction = instructions[i];
+      const matches = regex.exec(instruction);
+      if (matches === null) {
+        continue;
+      }
+
+      const [_, number, from, to] = matches;
+      const fromStack = stacks[parseInt(from) - 1];
+      const toStack = stacks[parseInt(to) - 1];
+      const temp = fromStack.splice(fromStack.length - parseInt(number));
+      console.log(temp);
+
+      toStack.push(...temp);
+    }
+
+    let result = "";
+    for (let index = 0; index < stacks.length; index++) {
+      const stack = stacks[index];
+      if (stack.length === 0) {
+        continue;
+      }
+      result += stack.pop() ?? "";
+    }
+
+    return result;
   }
 }
 
